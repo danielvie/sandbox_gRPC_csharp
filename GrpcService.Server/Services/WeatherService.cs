fefd44a522488c7461405092497899bba0e3901c
Daniel Vieira <danielvie@gmail.com>
2022-06-20 23:17:15 +0200

    grpc - part 1

---
+++ GrpcService.Server/Services/WeatherService.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Net.Http;
+using Grpc.Core;
+using System.Threading.Tasks;
+using System.Text.Json;
+using GrpcService.Server.Contracts;
+
+namespace GrpcService.Server.Services
+{
+    public class WeatherService : Server.WeatherService.WeatherServiceBase
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public WeatherService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+                
+        public override async Task<WeatherResponse> GetCurrentWeather(
+            GetCurrentWeatherForCityRequest request, 
+            ServerCallContext context)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var responseText = await httpClient.GetStringAsync(
+                $"https://api.openweathermap.org/data/2.5/weather?q={request.City}&appid=8249b2ce8322fa7479c5f5e89a32eb71&units={request.Units}"
+            );
+
+            var temperatures = JsonSerializer.Deserialize<Temperatures>(responseText);
+
+            return new WeatherResponse
+            {
+                Temperature = temperatures!.Main.Temp,
+                FeelsLike = temperatures.Main.FeelsLike,
+                Lat = temperatures.Coord.Lat,
+                Lon = temperatures.Coord.Lon
+            };
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+
+## REFERENCIA
+
+site C# models :: https://quicktype.io/csharp
+
+
+youtube :: https://www.youtube.com/watch?v=hp5FTB7PI9s
+    gRPC Server & Unary Calls | gRPC in .NET 5
+    [Nick Chapsas]
